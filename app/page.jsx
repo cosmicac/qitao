831254f8e6e1761cf746492e9131a9efdb68edae
Tom Li <weirdotomli@gmail.com>
2025-01-19 00:14:18 -0800

    Spin the ball.

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import dynamic from "next/dynamic";
-import Section from "@/components/text/section";
-import Spinner from "@/components/spinner";
+
 import { Text3D, Center } from "@react-three/drei";
+
+import About from "@/components/about";
+import Spinner from "@/components/spinner";
 
 const Basketball = dynamic(
   () => import("@/components/canvas/basketball").then((mod) => mod.Basketball),
@@ -21,52 +23,31 @@ const Common = dynamic(() => import("@/components/canvas/View").then((mod) => mo
 });
 
 const Home = () => {
-  const [showText, setShowText] = useState(true);
-
   return (
     <>
       <div className="flex flex-col gap-4">
-        <Section>
-          <div className="text-3xl font-bold pb-4">Qitao (Tom) Li</div>
-          <div>
-            I&apos;m Tom, a software engineer based in the San Francisco Bay Area. I currently work
-            at Affinity.
-          </div>
-          <div>
-            I&apos;m a generalist at heart, but my skillset leans backend. I like to put my product
-            hat on and collaborate closely with product and design.
-          </div>
-          <div>
-            In my spare time, I like to watch basketball (go Warriors & Valkyries!), play video
-            games, and work on various side projects.
-          </div>
-        </Section>
+        <About />
         <View orbit className="h-96">
           <Suspense fallback={null}>
-            {showText && (
-              <Center position={[0, 1.1, 0]} rotation={[-0.25, -0.2, 0]}>
-                <Text3D
-                  font={"/inter_3d.json"}
-                  curveSegments={32}
-                  bevelEnabled
-                  bevelSize={0.01}
-                  bevelThickness={0.05}
-                  height={0.02}
-                  lineHeight={0.5}
-                  letterSpacing={-0.01}
-                  size={0.2}
-                >
-                  go warriors!
-                  <meshNormalMaterial />
-                </Text3D>
-              </Center>
-            )}
-            <Basketball
-              scale={0.8}
-              position={[0, 0, 0]}
-              rotation={[1.0, 0.0, -0.3]}
-              onClick={() => setShowText(true)}
-            />
+            <Center position={[0, 0.3, 0]}>
+              <Basketball scale={0.8} position={[0, 0, 0]} rotation={[1.0, 0.0, -0.3]} />
+            </Center>
+            <Center position={[0, -0.8, 0]} rotation={[-0.5, -0.25, 0.0]}>
+              <Text3D
+                font={"/inter_3d.json"}
+                curveSegments={32}
+                bevelEnabled
+                bevelSize={0.01}
+                bevelThickness={0.05}
+                height={0.02}
+                lineHeight={0.5}
+                letterSpacing={-0.01}
+                size={0.2}
+              >
+                go warriors!
+                <meshNormalMaterial />
+              </Text3D>
+            </Center>
             <Common />
           </Suspense>
         </View>
---
+++ src/components/about.jsx
@@ -0,0 +1,23 @@
+import Section from "@/components/text/section";
+
+const About = () => {
+  return (
+    <Section>
+      <div className="text-3xl font-bold pb-4">Qitao (Tom) Li</div>
+      <div>
+        I&apos;m Tom, a software engineer based in the San Francisco Bay Area. I currently work at
+        Affinity.
+      </div>
+      <div>
+        I&apos;m a generalist at heart, but my skillset leans backend. I like to put my product hat
+        on and collaborate closely with product and design.
+      </div>
+      <div>
+        In my spare time, I like to watch basketball (go Warriors & Valkyries!), play video games,
+        and work on various side projects.
+      </div>
+    </Section>
+  );
+};
+
+export default About;
