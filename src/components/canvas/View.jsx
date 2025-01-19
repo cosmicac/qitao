c882736c5cc9b841fe8ceb0a1b5d13f4353de9d0
Tom Li <weirdotomli@gmail.com>
2025-01-19 00:40:37 -0800

    Mess with lighting.

--- src/components/canvas/View.jsx
+++ src/components/canvas/View.jsx
@@ -7,9 +7,9 @@ import { Three } from "@/helpers/components/Three";
 export const Common = ({ color }) => (
   <Suspense fallback={null}>
     {color && <color attach="background" args={[color]} />}
-    <ambientLight />
-    <pointLight position={[20, 30, 10]} intensity={3} decay={0.2} />
-    <pointLight position={[-10, -10, -10]} color="blue" decay={0.2} />
+    <ambientLight intensity={1} />
+    <pointLight position={[30, 30, 10]} color="gold" intensity={6} decay={0.1} />
+    <pointLight position={[-10, -10, -10]} color="blue" intensity={6} decay={0.1} />
     <PerspectiveCamera makeDefault fov={40} position={[0, 0, 4]} />
   </Suspense>
 );
